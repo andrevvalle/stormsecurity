44e9d8c531bb78635660d257e804e130f0de0ab6
Andre Valle <andrevvalle@gmail.com>
2014-08-16 21:48:54 -0300

    header and nav 90%

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-
+$(document).ready( function(){
+	var nav = $('#nav-toggle');
+	
+	nav.click( function(){
+		var base = $(this);
+		base.toggleClass('active');
+		$('#nav').slideToggle();
+	});
+});
