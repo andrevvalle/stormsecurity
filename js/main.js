f0ef66d0ab83d2d23f7c1aa3703e0a2239e63138
Andre Valle <andrevvalle@gmail.com>
2014-08-17 01:24:11 -0300

    header 100% and section1 40%

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 $(document).ready( function(){
 	var nav = $('#nav-toggle');
 	
-	nav.click( function(){
+	nav.click( function(e){
+	e.preventDefault();
 		var base = $(this);
+		
 		base.toggleClass('active');
 		$('#nav').slideToggle();
 	});
