6c3151bc9e162bc4829d119c34a09509f724c2d6
Andre Valle <andrevvalle@gmail.com>
2014-08-17 17:56:19 -0300

    section1 90%

--- js/main.js
+++ js/main.js
@@ -8,4 +8,55 @@ $(document).ready( function(){
 		base.toggleClass('active');
 		$('#nav').slideToggle();
 	});
+
 });
+// AJAX CONTENT
+function init() {
+
+	var primeiraurl = $('a.ajax:first-child').attr('href');
+	
+	// var urlnext = $('.ulajax li a.active').closest('li').next().find('a').attr('href');
+	// var urlprev = $('.ulajax li a.active').closest('li').prev().find('a').attr('href');
+
+	changePage(primeiraurl);
+
+	$(document).on('click', 'a.ajax', function(e){ 
+
+	$('.ulajax li a.active').removeClass('active');
+	$(this).addClass('active');
+
+	e.preventDefault();
+	var url = $(this).attr('href');
+	$('#appContent').fadeOut();
+	changePage(url);
+
+	});
+}
+function changePage(urlPage){
+
+    loading('open');
+
+    setTimeout(function(){
+      $.ajax({
+        type: 'GET',
+        url: urlPage,
+        context: $('#appContent'),
+        success: function(data){
+
+          this.html(data).fadeIn();
+          loading('close');
+
+        },
+        error: function(xhr, type){
+          console.log('erro no ajax');
+        }
+      })
+    }, 500);
+}
+function loading(acao){
+    if (acao == 'open') {
+        $('#loading').fadeIn('fast'); 
+    }else{
+        $('#loading').fadeOut('fast');
+    };
+}
